d79409e6abbcf08694595bb00409d596001f4ced
Seamus <m0ose01@proton.me>
2024-09-15 14:20:11 +0100

    Implement win detection (no diagonals yet).

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 use crate::board::{
-    Square,
     Board,
 };
 
@@ -8,7 +7,6 @@ const DIMENSIONS: usize = 2;
 
 fn main() {
     let mut board = Board::new(3);
-    let mut turn = 0;
     'game_loop: loop {
 
         println!("{board}");
@@ -50,30 +48,40 @@ fn main() {
 
         let coords = (coords[0], coords[1]);
 
-        let current_piece =  match turn % 2 == 0 {
-            true => Square::X,
-            false => Square::O,
-        };
-
-        match board.place_piece(current_piece, coords) {
+        match board.place_piece(coords) {
             Ok(_) => println!("Placed piece at {}, {}", coords.0, coords.1),
             Err(_) => {
                 println!("Cannot place a piece there.");
                 continue 'game_loop;
             }
         }
-        turn += 1;
+
+        let win_threshold = 3;
+        match board.check_status(win_threshold) {
+            board::BoardStatus::Win(player) => {
+                println!("{board}");
+                println!("{player} won!");
+                break 'game_loop;
+            },
+            board::BoardStatus::Draw => {
+                println!("The game ended in a draw");
+                break 'game_loop;
+            },
+            board::BoardStatus::None => (),
+        }
     }
 }
 
 mod board {
     use ndarray::{
+        Ix1,
         Ix2,
         Array,
+        ArrayView,
     };
     use std::fmt;
 
-    #[derive(Clone)]
+    #[derive(Clone, PartialEq)]
     pub enum Square {
         X,
         O,
@@ -82,7 +90,14 @@ mod board {
 
     pub struct Board {
         pub size: usize,
-        board: Array<Square, Ix2>
+        board: Array<Square, Ix2>,
+        turn: usize,
+    }
+
+    pub enum BoardStatus {
+        None,
+        Draw,
+        Win(Square),
     }
 
     impl Board {
@@ -90,19 +105,56 @@ mod board {
             Board {
                 size,
                 board: Array::<Square, Ix2>::from_elem((size, size), Square::Blank),
+                turn: 0,
             }
         }
 
-        pub fn place_piece(&mut self, piece: Square, position: (usize, usize)) -> Result<(), ()> {
+        pub fn place_piece(&mut self, position: (usize, usize)) -> Result<(), ()> {
+            let piece = if self.turn % 2 == 0 {Square::X} else {Square::O};
             let square = &mut self.board[(position.0, position.1)];
             match square {
                 Square::Blank => {
                     *square = piece;
+                    self.turn += 1;
                     Ok(())
                 },
                 _ => Err(())
             }
         }
+
+        pub fn check_status(&self, win_threshold: usize) -> BoardStatus {
+            let winner = if self.turn % 2 == 0 {Square::O} else {Square::X};
+
+            for current_row in self.board.rows() {
+                if check_lane(current_row, win_threshold) {
+                    return BoardStatus::Win(winner)
+                }
+            }
+
+            for current_col in self.board.columns() {
+                if check_lane(current_col, win_threshold) {
+                    return BoardStatus::Win(winner)
+                }
+            }
+
+            if self.turn >= usize::pow(self.size, 2) {
+                return BoardStatus::Draw;
+            }
+
+            return BoardStatus::None
+        }
+    }
+
+    fn check_lane(lane: ArrayView<Square, Ix1>, win_threshold: usize) -> bool {
+        for current_chunk in lane.windows(win_threshold) {
+            if current_chunk.iter().any(|x| *x == Square::Blank) {
+                continue;
+            }
+            if current_chunk.iter().all(|x| *x == current_chunk[0]) {
+                return true;
+            }
+        }
+        return false
     }
 
     impl fmt::Display for Board {
